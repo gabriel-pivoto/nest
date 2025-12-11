90e6dd14087e5651080b5875ad0f6e93f124442a
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-12-11 16:32:28 -0300

    first test

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,43 @@
-test('1 plus 1', () =>{
-    expect(1+1).equal(2)
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { randomUUID } from 'crypto'
+
+describe('Create Account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const email = `user-${randomUUID()}@example.com`
+
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
 })
